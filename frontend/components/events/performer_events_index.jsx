96b6b459b549add1729acc331c747858b2f5699d
Jyot Patel <jyot.95patel@gmail.com>
2018-07-17 17:16:44 -0400

    Add events to performer-events index with styling

--- frontend/components/events/performer_events_index.jsx
+++ frontend/components/events/performer_events_index.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import {
   requestPerformer
 } from '../../actions/performer_actions';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter, Link, Redirect } from 'react-router-dom';
 import PerformerEventsIndexItem from './performer_events_index_item';
 
 class PerformerEventsIndex extends React.Component {
@@ -19,14 +19,25 @@ class PerformerEventsIndex extends React.Component {
 
   render() {
     if (!this.props.performer) return null;
-    const eventLinks = this.props.events.map(event => <li key={event.id}><Link to={`/events/${event.id}`}>{event.title}</Link></li>);
+
+    const eventLinks = this.props.events.map(event => {
+      return (
+        <li className="performer-event-list-item" key={event.id}>
+          <Link to={`/events/${event.id}`} >
+            <PerformerEventsIndexItem event={event} />
+          </Link>
+        </li>
+      );
+    });
 
     return (
       <div className="performers-index-container" >
         <header className="main-content-splash">
           <div className="main-content-splash-info">
             <h3 className="main-content-route-info">
-              <span><Link to="/" >Home</Link> / <Link to={`/categories/${this.props.performer.category}`} >{this.props.performer.category}</Link> / {this.props.performer.name}</span>
+              <span>
+                <Link to="/" >Home</Link> / <Link to={`/categories/${this.props.performer.category}`} >{this.props.performer.category}</Link> / {this.props.performer.name}
+              </span>
             </h3>
             <h1 className="main-content-splash-name">{this.props.performer.name} Tickets</h1>
           </div>
--- frontend/components/events/performer_events_index_item.jsx
+++ frontend/components/events/performer_events_index_item.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const PerformerEventsIndexItem = ({event}) => {
-  return (
-    null
-  );
+class PerformerEventsIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.history.push(`/events/${this.props.event.id}`)
+  }
+
+  render() {
+    const eventOn = this.props.event.eventOn.split(" ");
+    const day = eventOn[0];
+    const date = eventOn.slice(1, 3).join(" ");
+    const time = eventOn.slice(4, 6).join(" ");
+
+    return (
+      <div className="performer-event-item-container">
+        <div className="performer-event-item-event-on" >
+          <p className="performer-event-date">{date}</p>
+          <p className="performer-event-time">{day} · {time}</p>
+        </div>
+        <div className="performer-event-info">
+          <p className="performer-event-title">{this.props.event.title}</p>
+        </div>
+        <div className="performer-event-button">
+          <button className="event-show-button" onClick={this.handleClick}>From $105</button>
+        </div>
+      </div>
+    );
+  }
 };
 
-export default PerformerEventsIndexItem;
+export default withRouter(PerformerEventsIndexItem);
