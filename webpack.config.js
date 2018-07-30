6cf2e655a330f0269ffb01635c746b8db7b5529b
Jyot Patel <jyot.95patel@gmail.com>
2018-07-30 17:29:12 -0400

    Remove logger from production environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
-var path = require("path");
+const path = require("path");
+const webpack = require("webpack");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+let plugins = []; // if using any plugins for both dev and production
+let devPlugins = []; // if using any plugins for development
+
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
@@ -6,6 +23,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
+  },
+  plugins: plugins,
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: true
+        }
+      })
+    ]
   },
   module: {
     rules: [
